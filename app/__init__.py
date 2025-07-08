9af4837355bcff2bff58d113fb7f36c2b3be078c
Gideon1712 <gioketunji@gmail.com>
2025-07-09 00:33:09 +0100

    new __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -8,16 +8,23 @@ db = SQLAlchemy()
 
 def create_app():
     load_dotenv()
-    app = Flask(__name__)
+
+
+    app = Flask(
+        __name__,
+        template_folder='templates',
+        static_folder='static'
+    )
+
     CORS(app)
-    
+
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    # ✅ this part is key!
+    # ✅ Register your main blueprint
     from .routes import main
     app.register_blueprint(main)
 
