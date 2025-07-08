33170e8bba0c6eb95fc352a1395455c9038c4de9
Gideon1712 <gioketunji@gmail.com>
2025-07-09 00:30:07 +0100

    newer run.py file

--- run.py
+++ run.py
@@ -1,15 +1,6 @@
-from flask import Flask, send_from_directory
 from app import create_app
 
 app = create_app()
-
-@app.route('/')
-def serve_frontend():
-    return send_from_directory('frontend', 'index.html')
-
-@app.route('/<path:path>')
-def serve_static_file(path):
-    return send_from_directory('frontend', path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
