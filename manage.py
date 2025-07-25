0cecffc8ac503d26b7b7701974cdabd1eac82e6f
Gideon1712 <gioketunji@gmail.com>
2025-07-25 12:20:14 +0100

    changes

--- manage.py
+++ manage.py
@@ -2,10 +2,15 @@ from flask_migrate import Migrate
 from flask.cli import FlaskGroup
 from app import create_app, db
 
+# Create app
 app = create_app()
+
+# Bind Migrate to app and db
 migrate = Migrate(app, db)
 
-cli = FlaskGroup(app=app)  # DO NOT use create_app=... here
+# Register Flask CLI group
+cli = FlaskGroup(app=app)  # ✅ NOT create_app=create_app
 
+# Expose CLI when this file is run
 if __name__ == '__main__':
     cli()
