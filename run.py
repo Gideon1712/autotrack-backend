3bd66cbb5b6392ea068fa75bc2c767bf837530e7
Gideon1712 <gioketunji@gmail.com>
2025-07-25 05:17:49 +0100

    newer changes

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
-from app import create_app, db
+from flask import Flask
 from flask_migrate import Migrate
+from app import create_app, db  # ⬅️ import db from __init__.py
 
 app = create_app()
-
-# Register Migrate after app + db are ready
-migrate = Migrate(app, db)
+migrate = Migrate(app, db)  # ⬅️ Connect migrate to app and db
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
