baba3f04dcbf1ee88bc768c7a2d86ddb410cbf17
Gideon1712 <gioketunji@gmail.com>
2025-07-26 09:00:30 +0100

    changes

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
-# manage.py
 from flask.cli import FlaskGroup
 from app import create_app, db
 
+app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
