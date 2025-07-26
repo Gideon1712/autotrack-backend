b2f167bb597b52396cb7f6da1c68cec1ea86eef5
Gideon1712 <gioketunji@gmail.com>
2025-07-26 09:59:02 +0100

    changes

--- manage.py
+++ manage.py
@@ -1,7 +1,11 @@
 from flask.cli import FlaskGroup
 from app import create_app, db
 
-cli = FlaskGroup(create_app=create_app)
+# The app factory pattern
+def create():
+    return create_app()
+
+cli = FlaskGroup(create_app=create)
 
 if __name__ == "__main__":
     cli()
